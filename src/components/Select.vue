0445d38c1fb78604d7713545c5ababe8e42e9dd7
hsimao <e087754958@gmail.com>
2018-08-05 20:58:20 +0800

    edit Select UI component

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <div class="select">
-      <div class="title" @click="selectToggle(true)">{{title}}</div>
+    <div class="select" :class="{active: isShow}">
+      <div v-if="!current" class="title desc" @click="selectToggle(true)">{{title}}</div>
+      <div v-else class="title film" @click="selectToggle(true)">{{selectName}}</div>
       <ul :class="{active: isShow}" @mouseleave="selectToggle(false)">
         <li v-for="item in items" :key="item" :class="{active: current === item}" @mouseover="updateAction($event)" @click="selectToggle(false)" :data-id="item">{{item}}</li>
       </ul>
@@ -16,7 +17,8 @@ export default {
   data() {
     return {
       isShow: false,
-      current: null
+      current: null,
+      selectName: null
     }
   },
   methods: {
@@ -24,7 +26,7 @@ export default {
       this.isShow = type ? true : false
     },
     updateAction(el) {
-      this.title = el.target.innerText
+      this.selectName = el.target.innerText
       if (this.current !== null) {
         this.current
       }
@@ -35,38 +37,58 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+.select.active
+  &:before
+    transform: rotate(0deg)
 .select
   position: relative
   width: 150px
-  font-size: 16px
+  font-size: 15px
   cursor: pointer
   &:before
     content: '\f3d0'
     font-family: 'Ionicons'
     position: absolute
     right: 12px
-    height: 36px
-    line-height: 36px
+    height: 45px
+    line-height: 45px
+    transform: rotate(180deg)
+    transition: all .3s
   .title, li
-    height: 36px
-    line-height: 36px
+    height: 45px
+    line-height: 45px
     padding-left: 16px
     width: 126px
     @extend %moreText
   .title
     width: 126px
-    color: $colorPrimary
   ul
     display: none
     position: absolute
+    top: 47px
     width: 100%
+    height: 400px
+    border-right: solid 3px $colorPrimary
+    border-bottom: solid 3px $colorPrimary
+    border-left: solid 1px $colorPrimary
+    box-shadow: 0 4px 6px 0 rgba(black, 0.2)
+    overflow-y: auto
   ul.active
     display: block
   li
+    position: relative
     width: 100%
-    background-color: $colorGray
+    background-color: rgba($colorGray, 0.9)
     font-weight: 300
     transition: all .3s
+    &:after
+      content: ''
+      position: absolute
+      bottom: 0px
+      left: 0
+      width: 100%
+      height: 1px
+      background-color: rgba($colorWhite, 0.3)
   li.active
     background-color: $colorPrimary
     transform: translate(-2px, -2px)
